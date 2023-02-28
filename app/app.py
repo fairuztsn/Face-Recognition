cd3cabf291991f7e46fb32ee350e95f4ccd35038
f3iru <this.fairuz@gmail.com>
2023-02-28 09:52:15 +0700

    add ssl_context

--- app/app.py
+++ app/app.py
@@ -146,4 +146,4 @@ def tempsession():
 
 if __name__=='__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True, ssl_context="adhoc")
